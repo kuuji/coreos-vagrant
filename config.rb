95c350a339fbd050375bbc2265d73b8c89296094
Yan MATAGNE <ymatagne@norsys.fr>
2016-01-09 17:56:59 +0100

    init(core) : Change and commit user-data

--- config.rb
+++ config.rb
@@ -1,5 +1,5 @@
 # Size of the CoreOS cluster created by Vagrant
-$num_instances=1
+$num_instances=5
 
 # Used to fetch a new discovery token for a cluster of size $num_instances
 $new_discovery_url="https://discovery.etcd.io/new?size=#{$num_instances}"
@@ -43,17 +43,17 @@ end
 # Change basename of the VM
 # The default value is "core", which results in VMs named starting with
 # "core-01" through to "core-${num_instances}".
-#$instance_name_prefix="core"
+$instance_name_prefix="core"
 
 # Change the version of CoreOS to be installed
 # To deploy a specific version, simply set $image_version accordingly.
 # For example, to deploy version 709.0.0, set $image_version="709.0.0".
 # The default value is "current", which points to the current version
 # of the selected channel
-#$image_version = "current"
+$image_version = "current"
 
 # Official CoreOS channel from which updates should be downloaded
-#$update_channel='alpha'
+$update_channel='alpha'
 
 # Log the serial consoles of CoreOS VMs to log/
 # Enable by setting value to true, disable with false
@@ -75,7 +75,7 @@ end
 
 # Customize VMs
 #$vm_gui = false
-#$vm_memory = 1024
+$vm_memory = 512
 #$vm_cpus = 1
 
 # Share additional folders to the CoreOS VMs
