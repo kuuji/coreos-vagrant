1b133b664094ad32a13a670c7d3e8f80b564a54f
Alexandre GARCIA <agarcia@norsys.fr>
2016-01-11 18:54:19 +0100

    remove fucked up usage

--- units/trooper.sh
+++ units/trooper.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
-usage(){
-	echo "Usage: $0 first-id last-id"
-	exit 1
-}
-
-
-
-# call usage() function if filename not supplied
-[[ $# -eq 1 ]] && usage
 
 for ((c=$1; c<=$2; c++))
 do
