0f4946cdaa389acd0fe8e10ca274644c00d85b3d
Alexandre GARCIA <agarcia@norsys.fr>
2016-01-12 16:30:55 +0100

    rename init.sh to init.env

--- units/trooper-delete.sh
+++ units/trooper-delete.sh
@@ -5,5 +5,4 @@ do
    echo $c
    fleetctl --tunnel=172.17.8.101  stop trooper@$c
    fleetctl --tunnel=172.17.8.101  destroy trooper@$c
-   fleetctl --tunnel=172.17.8.101  destroy sidekick@$c
 done
